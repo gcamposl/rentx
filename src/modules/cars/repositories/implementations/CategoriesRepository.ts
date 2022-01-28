4f1f3dcfcebeb3e66b83c359a10dffa298584fc5
Gabriel <gcamposlopes@gmail.com>
2022-01-27 23:07:35 -0300

    Separando os repositórios

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,5 +1,5 @@
-import { Category } from "../model/Category";
-import { ICategoryRepository, ICreateCategoryDTO } from "./ICategoryRepository";
+import { Category } from "../../model/Category";
+import { ICategoryRepository, ICreateCategoryDTO } from "../ICategoryRepository";
 
 //DTO -> Data Transfer Object
 
--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,5 +1,5 @@
-import { Specification } from "../model/Specification";
-import { ISpecificationsRepository, ICreateSpecificationDTO } from "./ISpecifitcationsRepository";
+import { Specification } from "../../model/Specification";
+import { ISpecificationsRepository, ICreateSpecificationDTO } from "../ISpecifitcationsRepository";
 
 
 class SpecificationsRepository implements ISpecificationsRepository {
