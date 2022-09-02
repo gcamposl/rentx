5b652527333ef345fc24a0ab50a7818cd8a3c9d1
gcamposl <gcamposlopes@gmail.com>
2022-09-02 12:28:37 -0300

    feat: get users by email

--- src/modules/accounts/UseCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/UseCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcryptjs';
 import { inject, injectable } from 'tsyringe';
 
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
@@ -16,9 +17,14 @@ class CreateUserUseCase {
     email,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const passwordHash = await hash(password, 8);
+    const userAlredyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlredyExists) throw new Error('User alredy exists');
+
     await this.usersRepository.create({
       name,
-      password,
+      password: passwordHash,
       email,
       driver_license,
     });
